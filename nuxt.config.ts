93661e80bfc490b42c11bf3c83f7537fc5ac7bc3
Víctor Campos <portadict@hotmail.com>
2024-09-29 00:17:33 +0200

    does this fix chartjs

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -6,19 +6,23 @@ export default defineNuxtConfig({
   modules: ["@nuxtjs/tailwindcss"],
 
   // Add a main.scss file in the assets directory
-  css: [
-    "~/assets/main.scss",
-    "~/assets/icons.scss",
-  ],
+  css: ["~/assets/main.scss", "~/assets/icons.scss"],
 
   // Add link styles to head of the app
   app: {
     head: {
       link: [
         { rel: "preconnect", href: "https://fonts.googleapis.com" },
-        { rel: "preconnect", href: "https://fonts.gstatic.com", crossorigin: "anonymous" },
-        { rel: "stylesheet", href: "https://fonts.googleapis.com/css2?family=Dosis:wght@200..800&family=Indie+Flower&display=swap" }
-      ]
-    }
-  }
+        {
+          rel: "preconnect",
+          href: "https://fonts.gstatic.com",
+          crossorigin: "anonymous",
+        },
+        {
+          rel: "stylesheet",
+          href: "https://fonts.googleapis.com/css2?family=Dosis:wght@200..800&family=Indie+Flower&display=swap",
+        },
+      ],
+    },
+  },
 });
